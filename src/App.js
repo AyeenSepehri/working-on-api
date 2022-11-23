34abd397f3bca7b480a5046dae30b35b09b237c7
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-11-23 17:36:00 +0330

    adding loading status and a fallback content

--- src/App.js
+++ src/App.js
@@ -6,23 +6,24 @@ import './App.css';
 function App() {
 
   const [movie, setMovie] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
   const fetchDataHandler = async () => {
+    setIsLoading(true)
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
-        const transformedData = data.results.map(
-          (movieData) => {
-            return {
-              id: movieData.episode_id,
-              title: movieData.title,
-              releaseDate: movieData.release_date,
-              openingText: movieData.opening_crawl
-            }
-          }
-        )
-        setMovie(transformedData)
-      
-    
+    const transformedData = data.results.map(
+      (movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl
+        }
+      }
+    )
+    setMovie(transformedData)
+    setIsLoading(false)
   }
 
   return (
@@ -31,7 +32,9 @@ function App() {
         <button onClick={fetchDataHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movie} />
+        {!isLoading && movie.length === 0 && <p>movie not found!</p>}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && movie.length > 1 && <MoviesList movies={movie} />}
       </section>
     </React.Fragment>
   );
