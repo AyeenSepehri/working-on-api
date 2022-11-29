78ff589947a71b1ddb33853e5d17feffe4b017a5
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-11-29 14:33:07 +0330

    set error message for unsuccessful fetch api

--- src/App.js
+++ src/App.js
@@ -7,24 +7,37 @@ function App() {
 
   const [movie, setMovie] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
 
   const fetchDataHandler = async () => {
     setIsLoading(true)
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedData = data.results.map(
-      (movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          releaseDate: movieData.release_date,
-          openingText: movieData.opening_crawl
-        }
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("fetch is not ok")
       }
-    )
-    setMovie(transformedData)
+      const data = await response.json();
+      const transformedData = data.results.map(
+        (movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            releaseDate: movieData.release_date,
+            openingText: movieData.opening_crawl
+          }
+        }
+      )
+      setMovie(transformedData)
+      setIsLoading(false)
+    }
+    catch (e) {
+      setError(true)
+      setErrorMessage(e.message)
+    }
     setIsLoading(false)
   }
+
 
   return (
     <React.Fragment>
@@ -32,9 +45,10 @@ function App() {
         <button onClick={fetchDataHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movie.length === 0 && <p>movie not found!</p>}
+        {!isLoading && movie.length === 0 && !error && <p>movie not found!</p>}
         {isLoading && <p>Loading...</p>}
         {!isLoading && movie.length > 1 && <MoviesList movies={movie} />}
+        {error && !isLoading && <p>{errorMessage}</p>}
       </section>
     </React.Fragment>
   );
